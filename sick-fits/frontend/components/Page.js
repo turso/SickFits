9c76c18e38846234eacaa1308e8349f403ece874
Oliver Rikkilä <turso@olen.to>
2019-01-08 11:38:49 +0200

    first commit

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 100px;
+`;
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        <MyButton>Click me</MyButton>
+        {this.props.children}
+      </div>
+    );
+  }
+}
