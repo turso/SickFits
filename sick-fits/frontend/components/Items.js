ed81c7cab9f1cd07e7bf77004d6cb83491f30b39
Oliver Rikkilä <turso@olen.to>
2019-01-14 12:37:30 +0200

    pagination working with only 4 items showing on the page

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -7,8 +7,8 @@ import Pagination from './Pagination';
 import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY($skip: Int = 0) {
-    items(skip: $skip, orderBy: createdAt_DESC) {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -36,9 +36,9 @@ export default class Items extends Component {
         <Pagination page={this.props.page} />
         <Query
           query={ALL_ITEMS_QUERY}
-          // fetchPolicy="network-only"
           variables={{
-            skip: this.props.page * perPage - perPage
+            skip: this.props.page * perPage - perPage,
+            first: perPage
           }}
         >
           {({ data, error, loading }) => {
