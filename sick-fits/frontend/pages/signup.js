3671b689d4d055e405bc03a6510c8821e591da4d
Oliver Rikkilä <turso@olen.to>
2019-01-14 16:41:15 +0200

    signup frontend page and login working

---
+++ sick-fits/frontend/pages/signup.js
@@ -0,0 +1,18 @@
+import Signup from '../components/SignUp';
+import styled from 'styled-components';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+const SignUpPage = props => (
+  <Columns>
+    <Signup />
+    <Signup />
+    <Signup />
+  </Columns>
+);
+
+export default SignUpPage;
