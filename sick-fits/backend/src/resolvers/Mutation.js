926dd96dafe2187d0dd6fd6a3f7ddfe6f7a7cb0f
Oliver Rikkilä <turso@olen.to>
2019-01-16 14:29:12 +0200

    sign out functionality

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -94,6 +94,11 @@ const Mutations = {
 
     // 5. Return the user
     return user;
+  },
+
+  async signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
   }
 };
 
