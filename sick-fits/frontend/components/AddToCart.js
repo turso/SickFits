34df1d6a3ac9ab800fc9fa2dc6cf0804f2fabc29
Oliver Rikkilä <turso@olen.to>
2019-01-22 15:16:45 +0200

    server side add to cart functionality

---
+++ sick-fits/frontend/components/AddToCart.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const ADD_TO_CART_MUTATION = gql`
+  mutation addToCart($id: ID!) {
+    addToCart(id: $id) {
+      id
+      quantity
+    }
+  }
+`;
+
+export default class AddToCart extends Component {
+  render() {
+    const { id } = this.props;
+    return (
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{
+          id
+        }}
+      >
+        {addToCart => <button onClick={addToCart}>Add to Cart</button>}
+      </Mutation>
+    );
+  }
+}
